93716ce8b9f6d6dd3848f6795f7766f2bd3d2698
Ryan Pavlik <ryan@sensics.com>
2016-03-07 10:16:12 -0600

    Update scripts to match HDK scripts

--- Inf/generate-cat.cmd
+++ Inf/generate-cat.cmd
@@ -8,10 +8,10 @@ pushd "%~dp0"
 rem Remove old build products
 del *.cat *.htm > nul
 
-"%WDK_DIR%\Tools\x86\infverif" *.inf /v /l .
-start *.inf.htm
+rem "%WDK_DIR%\Tools\x86\infverif" *.inf /v /l .
+rem start *.inf.htm
 
-call "%WDK_DIR%\Tools\x86\chkinf\chkinf" *.inf
+rem call "%WDK_DIR%\Tools\x86\chkinf\chkinf" *.inf
 
 "%WDK_DIR%\bin\x86\inf2cat" /driver:. /os:6_3_X86,6_3_X64,6_3_ARM,Server6_3_X64,8_X64,8_X86,8_ARM,Server8_X64,Server2008R2_X64,Server2008R2_IA64,7_X64,7_X86,Server2008_X64,Server2008_IA64,Server2008_X86,Vista_X64,Vista_X86,Server2003_X64,Server2003_IA64,Server2003_X86,XP_X64,XP_X86
 
--- Inf/update-timestamp-and-version.cmd
+++ Inf/update-timestamp-and-version.cmd
@@ -2,11 +2,13 @@ rem Load the shared variable definitions.
 call "%~dp0..\shared.cmd"
 pushd "%~dp0"
 
+set MAJOR_VERSION_OVERRIDE=10
+
 rem Remove old build products - cat invalidated by timestamp change.
 del *.cat *.htm > nul
 
-rem Update the date to today and the version to 7.whateveryouentered
-"%WDK_DIR%\bin\x86\stampinf" -f sensics_cdc.inf -d * -v 7.%DRIVER_VER%
+rem Update the date to today and the version to MAJOR_VERSION_OVERRIDE.whateveryouentered
+"%WDK_DIR%\bin\x86\stampinf" -f sensics_cdc.inf -d * -v %MAJOR_VERSION_OVERRIDE%.%DRIVER_VER%
 
 popd
 pause
