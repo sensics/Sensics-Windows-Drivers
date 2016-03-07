7568541d513abb4e30908905c7264a45d3bc86a0
Ryan Pavlik <ryan@sensics.com>
2016-03-07 14:15:01 -0600

    Be able to sign the sensics combined installer

--- Combined-Installer/sign-combined-installer.cmd
+++ Combined-Installer/sign-combined-installer.cmd
@@ -4,6 +4,6 @@ call "%~dp0..\shared.cmd"
 
 pushd "%~dp0"
 
-"%WDK_DIR%\bin\x86\Signtool" sign /v /n %CERTNAME% /t %TIMESTAMP_SERVER% OSVR-HDK-Combined-Driver-Installer*.exe
+"%WDK_DIR%\bin\x86\Signtool" sign /v /n %CERTNAME% /t %TIMESTAMP_SERVER% Sensics-Combined-Driver-Installer*.exe
 
 popd
