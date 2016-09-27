2fe632edbcd1a2adf1236fa475ddf0e6f3b6734b
Ryan Pavlik <ryan@sensics.com>
2016-09-27 13:18:18 -0500

    Bump versions

--- Inf/update-timestamp-and-version.cmd
+++ Inf/update-timestamp-and-version.cmd
@@ -9,7 +9,7 @@ del *.cat *.htm > nul
 
 rem Update the date to today and the version to MAJOR_VERSION_OVERRIDE.whateveryouentered
 "%WDK_DIR%\bin\x86\stampinf" -f sensics_cdc.inf -d * -v %MAJOR_VERSION_OVERRIDE%.%DRIVER_VER%
-s
+
 rem Update the date to today and the version to 1.whateveryouentered
 "%WDK_DIR%\bin\x86\stampinf" -f sensics_display.inf -d * -v 1.%DRIVER_VER%
 
