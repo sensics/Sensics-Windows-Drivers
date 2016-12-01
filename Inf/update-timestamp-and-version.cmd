49d0e36970c710cf4c392eef7a29e748ff87ca2d
Ryan Pavlik <ryan@sensics.com>
2016-12-01 15:34:53 -0600

    Update the timestamp/version stamp script to have display major versions be 2

--- Inf/update-timestamp-and-version.cmd
+++ Inf/update-timestamp-and-version.cmd
@@ -10,8 +10,9 @@ del *.cat *.htm > nul
 rem Update the date to today and the version to MAJOR_VERSION_OVERRIDE.whateveryouentered
 "%WDK_DIR%\bin\x86\stampinf" -f sensics_cdc.inf -d * -v %MAJOR_VERSION_OVERRIDE%.%DRIVER_VER%
 
-rem Update the date to today and the version to 1.whateveryouentered
-"%WDK_DIR%\bin\x86\stampinf" -f sensics_display.inf -d * -v 1.%DRIVER_VER%
+rem Update the date to today and the version to 2.whateveryouentered
+rem This ensures that when conflicting, displays show up as Sensics Display rather than OSVR HDK
+"%WDK_DIR%\bin\x86\stampinf" -f sensics_display.inf -d * -v 2.%DRIVER_VER%
 
 popd
 pause
